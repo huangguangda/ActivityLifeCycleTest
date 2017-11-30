7506de7c58c1c9ec6bf99f5ca5b693c343f42c11
huangguangda <2397923107@qq.com>
2017-11-30 23:00:28 +0800

    制定主活动布局完成

--- app/src/main/java/cn/edu/gdmec/android/activitylifecycletest/DialogActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activitylifecycletest/DialogActivity.java
@@ -6,6 +6,12 @@ import android.view.Window;
 
 /**
  * Created by Jack on 2017/11/30.
+ * 活动的启动模式
+ * standard,singleTop,singleTask,singleInstance。
+ * Log.d("FirstActivity",this.toString());
+ * android:launchMode="singleTop"
+ * android:launchMode="singleTask"
+ * android:launchMode="singleInstance"
  */
 
 public class DialogActivity extends Activity{
