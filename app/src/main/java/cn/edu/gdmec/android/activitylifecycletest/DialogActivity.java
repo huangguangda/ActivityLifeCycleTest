897c4fdeb7c4ced3f133858f7d45fefd47f14da2
huangguangda <2397923107@qq.com>
2017-11-30 23:13:35 +0800

    制定主活动布局完成ok82

--- app/src/main/java/cn/edu/gdmec/android/activitylifecycletest/DialogActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activitylifecycletest/DialogActivity.java
@@ -12,6 +12,14 @@ import android.view.Window;
  * android:launchMode="singleTop"
  * android:launchMode="singleTask"
  * android:launchMode="singleInstance"
+ *
+ * Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+ * intent.putExtra("param1","data1");
+ * intent.putExtra("param2","data2");
+ * startActivity(intent);
+ *
+ * SecondActivity.actionStart(FirstActivity.this, "data1", "data2");
+ *
  */
 
 public class DialogActivity extends Activity{
